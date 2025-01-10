b6dcbc6a77408bd708c0cb03e3d749a6afc9e173
Cruz Núñez <cruz@Cruzs-15-MacBook-Pro.local>
2025-01-10 17:11:01 -0500

    add Hamilton County Solid Waste Policy Committee spider

---
+++ city_scrapers/spiders/cinoh_hamilton_solid_waste.py
@@ -0,0 +1,80 @@
+from urllib.parse import urljoin
+
+from city_scrapers_core.constants import COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse
+
+
+class CinohHamiltonSolidWasteSpider(CityScrapersSpider):
+    name = "cinoh_hamilton_solid_waste"
+    agency = "Hamilton County Solid Waste Policy Committee"
+    timezone = "America/New_York"
+    start_urls = ["https://www.hcdoes.org/AgendaCenter/"]
+    location = {
+        "name": "Hamilton CountyY Environmental Services",
+        "address": "250 William Howard Taft Road, First Floor, Cincinnati, OH 45219",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for item in response.css("tbody tr"):
+            meeting = Meeting(
+                title="Solid Waste Policy Committee",
+                description="",
+                classification=COMMITTEE,
+                start=self._parse_start(item),
+                end=None,
+                all_day=False,
+                time_notes="",
+                location=self.location,
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, item):
+        """
+        Parse start datetime as a naive datetime object.
+        All the meetings in the agendas started at 3pm. Continue using 3pm.
+        """
+        text = item.css("strong::attr(aria-label)").get()
+        if " for " in text:
+            date = text.split(" for ")[1]
+            return parse(f"{date} 3pm")
+        else:
+            return None
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        return {
+            "address": "",
+            "name": "",
+        }
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        base_url = "https://www.hcdoes.org/"
+        output = []
+        links = item.css(".minutes a, .media a, .popout a")
+        for link in links:
+            # import pdb; pdb.set_trace()
+            title = link.css("::attr(aria-label)").get()
+            href = link.css("::attr(href)").get()
+            url = urljoin(base_url, href)
+            output.append({"title": title, "href": url})
+
+        return output
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
---
+++ tests/test_cinoh_hamilton_solid_waste.py
@@ -0,0 +1,171 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMITTEE
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cinoh_hamilton_solid_waste import (
+    CinohHamiltonSolidWasteSpider,
+)
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cinoh_hamilton_solid_waste.html"),
+    url="https://www.hcdoes.org/AgendaCenter/",
+)
+spider = CinohHamiltonSolidWasteSpider()
+
+freezer = freeze_time("2025-01-10")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 4
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Solid Waste Policy Committee"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2024, 11, 20, 15, 0)
+    assert parsed_items[3]["start"] == datetime(2024, 2, 21, 15, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == (
+        "cinoh_hamilton_solid_waste/202411201500/x/solid_waste_policy_committee"
+    )
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "passed"
+    assert parsed_items[3]["status"] == "passed"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Hamilton CountyY Environmental Services",
+        "address": "250 William Howard Taft Road, First Floor, Cincinnati, OH 45219",
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://www.hcdoes.org/AgendaCenter/"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [
+        {
+            "title": "November 20, 2024, November 20, 2024 Hamilton County Solid Waste"
+            " Policy Committee Information. Media",
+            "href": "https://youtu.be/7OE91Z8VOac",
+        },
+        {
+            "title": "November 20, 2024, November 20, 2024 Hamilton County Solid Waste"
+            " Policy Committee Information. Agenda",
+            "href": "https://www.hcdoes.org/AgendaCenter/ViewFile/Agenda/_11202024-40",
+        },
+        {
+            "title": "November 20, 2024, November 20, 2024 Hamilton County Solid Waste"
+            " Policy Committee Information. Previous Version",
+            "href": "https://www.hcdoes.org/AgendaCenter/PreviousVersions/_11202024-40",
+        },
+    ]
+    assert parsed_items[1]["links"] == [
+        {
+            "title": "August 14, 2024, This is information for the August 14, 2024 "
+            "Hamilton County Solid Waste Policy Committee meeting.. Minutes",
+            "href": "https://www.hcdoes.org/AgendaCenter/ViewFile/Minutes/_08142024-39",
+        },
+        {
+            "title": "August 14, 2024, This is information for the August 14, 2024 "
+            "Hamilton County Solid Waste Policy Committee meeting.. Media",
+            "href": "https://youtu.be/Jcx1t3pGSkI",
+        },
+        {
+            "title": "August 14, 2024, This is information for the August 14, 2024 "
+            "Hamilton County Solid Waste Policy Committee meeting.. Media",
+            "href": "https://youtu.be/Jcx1t3pGSkI?si=31ayi4XHkgVdctex&t=63",
+        },
+        {
+            "title": "August 14, 2024, This is information for the August 14, 2024 "
+            "Hamilton County Solid Waste Policy Committee meeting.. Agenda",
+            "href": "https://www.hcdoes.org/AgendaCenter/ViewFile/Agenda/_08142024-39",
+        },
+        {
+            "title": "August 14, 2024, This is information for the August 14, 2024 "
+            "Hamilton County Solid Waste Policy Committee meeting.. Previous Version",
+            "href": "https://www.hcdoes.org/AgendaCenter/PreviousVersions/_08142024-39",
+        },
+    ]
+    assert parsed_items[2]["links"] == [
+        {
+            "title": "May 15, 2024, May 15, 2024 Hamilton County Solid Waste Policy "
+            "Committee Meeting Information. Minutes",
+            "href": "https://www.hcdoes.org/AgendaCenter/ViewFile/Minutes/_05152024-38",
+        },
+        {
+            "title": "May 15, 2024, May 15, 2024 Hamilton County Solid Waste Policy "
+            "Committee Meeting Information. Media",
+            "href": "https://youtu.be/P8zwA4pitxU",
+        },
+        {
+            "title": "May 15, 2024, May 15, 2024 Hamilton County Solid Waste Policy "
+            "Committee Meeting Information. Agenda",
+            "href": "https://www.hcdoes.org/AgendaCenter/ViewFile/Agenda/_05152024-38",
+        },
+        {
+            "title": "May 15, 2024, May 15, 2024 Hamilton County Solid Waste Policy "
+            "Committee Meeting Information. Previous Version",
+            "href": "https://www.hcdoes.org/AgendaCenter/PreviousVersions/_05152024-38",
+        },
+    ]
+    assert parsed_items[3]["links"] == [
+        {
+            "title": "February 21, 2024, February 21, 2024 Hamilton County Solid Waste "
+            "Policy Committee Meeting Information. Minutes",
+            "href": "https://www.hcdoes.org/AgendaCenter/ViewFile/Minutes/_02212024-37",
+        },
+        {
+            "title": "February 21, 2024, February 21, 2024 Hamilton County Solid Waste "
+            "Policy Committee Meeting Information. Media",
+            "href": "https://youtu.be/NmJIkpmUC8c",
+        },
+        {
+            "title": "February 21, 2024, February 21, 2024 Hamilton County Solid Waste "
+            "Policy Committee Meeting Information. Agenda",
+            "href": "https://www.hcdoes.org/AgendaCenter/ViewFile/Agenda/_02212024-37",
+        },
+        {
+            "title": "February 21, 2024, February 21, 2024 Hamilton County Solid Waste "
+            "Policy Committee Meeting Information. Previous Version",
+            "href": "https://www.hcdoes.org/AgendaCenter/PreviousVersions/_02212024-37",
+        },
+    ]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == COMMITTEE
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
